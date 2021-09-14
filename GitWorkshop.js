7cbdc59fd72cb863b5144f7b1249d1c5c32d86e6
prathamesh-g-7 <prathameshgaikwad13999@gmail.com>
2021-09-14 18:23:54 +0530

    adding file through first commit

---
+++ GitWorkshop.js
@@ -0,0 +1,14 @@
+// This is sample file for git workshop
+
+let object1 = {
+  firstName: "Prathamesh",
+  lastName: "Gaikwad",
+  loginCount: 32,
+  isLogedInFromGoogle: true,
+};
+
+console.log(object1.firstName);
+console.log(object1);
+
+object1.loginCount = 50;
+console.table(object1);
